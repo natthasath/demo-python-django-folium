966f0075d2863e55e5072936d47a91bd26b5c76d
natthasath <natthasath.sak@gmail.com>
2023-08-28 09:19:49 +0700

    Add SSL and Update .env

--- app/core/settings.py
+++ app/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8&t6_9785sj&06k30i42r&maxpm!6+f#a0mjp4^iby%vdxtuon'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'module',
+    'sslserver',
 ]
 
 MIDDLEWARE = [
